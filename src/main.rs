3c4beb2925e07aa8bfa73f1a461f4332ba0d3c52
Andrew Vy <andrew@andrewvy.com>
2020-05-08 10:36:09 -0700

    :construction: Switch to Rocket for easier time.
    Async web frameworks are not yet here with the abstractions I want, and sync perf should be 'good enough'.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+use std::collections::HashMap;
 use std::env;
-use std::io::{self, Write};
 
-use warp::http::Response;
-use warp::{path, Filter, Rejection, Reply};
+use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::tera::{
+    Context as TeraContext, Result as TeraResult, Value as TeraValue,
+};
+use rocket_contrib::templates::Template;
+use serde::Serialize;
 
-use templates::RenderRucte;
+type GlobalFn = Box<dyn Fn(HashMap<String, TeraValue>) -> TeraResult<TeraValue> + Sync + Send>;
 
-#[derive(Debug)]
-#[allow(dead_code)]
+#[derive(Debug, Serialize)]
 pub struct BuildInfo {
     build_timestamp: String,
     build_date: String,
@@ -18,64 +23,37 @@ pub struct BuildInfo {
     semver: String,
 }
 
-#[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
-
-    let default_host = "127.0.0.1".to_owned();
-    let host = match env::var("HOST") {
-        Ok(val) => val,
-        Err(_) => default_host,
-    };
-
-    let default_port = 3030;
-    let port = match env::var("PORT") {
-        Ok(val) => match val.parse::<u16>() {
-            Ok(port) => port,
-            Err(_) => {
-                println!(
-                    "the port number \"{}\" is invalid. default port will be used.",
-                    val
-                );
-                default_port
-            }
-        },
-        Err(_) => {
-            println!("PORT is not defined in environment variables. default port will be used.",);
-            default_port
-        }
-    };
-
-    // GET /
-    let index = warp::get().and(warp::path::end().and_then(index));
-
-    // GET /public/:asset
-    let assets = warp::get().and(warp::path("public").and(warp::fs::dir("./build/")));
-
-    // All combined routes.
-    let routes = index.or(assets);
-
-    let addr: std::net::SocketAddr = format!("{}:{}", host, port).parse().unwrap();
-
-    warp::serve(routes).run(addr).await;
+#[rocket::get("/")]
+fn index() -> Template {
+    let context = TeraContext::new();
+    Template::render("index", &context)
 }
 
-async fn index() -> Result<impl Reply, Rejection> {
-    Response::builder().html(|o| templates::index(o))
+fn main() {
+    rocket::ignite()
+        .mount("/", rocket::routes![index])
+        .mount(
+            "/public",
+            StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/build")),
+        )
+        .attach(Template::custom(|engines| {
+            engines.tera.register_function("footer_data", footer_data());
+        }))
+        .launch();
 }
 
-fn footer(out: &mut dyn Write) -> io::Result<()> {
-    let build_info = BuildInfo {
-        build_timestamp: env::var("VERGEN_BUILD_TIMESTAMP").unwrap(),
-        build_date: env::var("VERGEN_BUILD_DATE").unwrap(),
-        sha: env::var("VERGEN_SHA").unwrap(),
-        sha_short: env::var("VERGEN_SHA_SHORT").unwrap(),
-        commit_date: env::var("VERGEN_COMMIT_DATE").unwrap(),
-        target_triple: env::var("VERGEN_TARGET_TRIPLE").unwrap(),
-        semver: env::var("VERGEN_SEMVER").unwrap(),
-    };
+fn footer_data() -> GlobalFn {
+    Box::new(move |_args| -> TeraResult<TeraValue> {
+        let build_info = BuildInfo {
+            build_timestamp: env::var("VERGEN_BUILD_TIMESTAMP").unwrap(),
+            build_date: env::var("VERGEN_BUILD_DATE").unwrap(),
+            sha: env::var("VERGEN_SHA").unwrap(),
+            sha_short: env::var("VERGEN_SHA_SHORT").unwrap(),
+            commit_date: env::var("VERGEN_COMMIT_DATE").unwrap(),
+            target_triple: env::var("VERGEN_TARGET_TRIPLE").unwrap(),
+            semver: env::var("VERGEN_SEMVER").unwrap(),
+        };
 
-    templates::footer(out, build_info)
+        Ok(serde_json::to_value(build_info).unwrap())
+    })
 }
-
-include!(concat!(env!("OUT_DIR"), "/templates.rs"));
