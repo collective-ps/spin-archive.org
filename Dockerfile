e03e8a7ea60ea92c7a45b6ea56d3c1378aa450dc
Andrew Vy <andrew@andrewvy.com>
2020-05-17 21:08:55 -0700

    :wrench: Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,58 @@
+# ------------------------------------------------------------------------------
+# Cargo Build Stage
+# ------------------------------------------------------------------------------
+
+FROM rust:latest as cargo-build
+
+RUN apt-get update
+
+WORKDIR /usr/src/spin-archive
+
+COPY Cargo.toml Cargo.toml
+
+RUN mkdir src/
+
+RUN echo "fn main() {println!(\"if you see this, the build broke\")}" > src/main.rs
+
+COPY rust-toolchain .
+
+RUN cargo build --release
+
+RUN rm -f target/release/deps/spin-archive*
+
+COPY . .
+
+RUN cargo build --release
+
+# ------------------------------------------------------------------------------
+# Front-end Assets Stage
+# ------------------------------------------------------------------------------
+
+FROM node:14-alpine as node-build
+
+WORKDIR /usr/src/spin-archive
+
+COPY package.json package.json
+COPY package-lock.json package-lock.json
+COPY webpack.config.js webpack.config.js
+COPY postcss.config.js postcss.config.js
+
+RUN npm install
+
+COPY assets assets
+
+RUN npm run build
+
+# ------------------------------------------------------------------------------
+# Final Stage
+# ------------------------------------------------------------------------------
+
+FROM cargo-build
+
+WORKDIR /home/spin-archive/bin/
+
+COPY --from=cargo-build /usr/src/spin-archive/target/release/spin-archive .
+COPY --from=cargo-build /usr/src/spin-archive/templates ./templates
+COPY --from=node-build /usr/src/spin-archive/build ./build
+
+CMD ["./spin-archive"]
--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ use rocket_contrib::templates::tera::{
     Context as TeraContext, Result as TeraResult, Value as TeraValue,
 };
 use rocket_contrib::templates::Template;
-use serde::Serialize;
 
 mod config;
 mod context;
@@ -36,18 +35,6 @@ use models::user::User;
 embed_migrations!();
 
 type GlobalFn = Box<dyn Fn(HashMap<String, TeraValue>) -> TeraResult<TeraValue> + Sync + Send>;
-
-#[derive(Debug, Serialize)]
-pub struct BuildInfo {
-    build_timestamp: String,
-    build_date: String,
-    sha: String,
-    sha_short: String,
-    commit_date: String,
-    target_triple: String,
-    semver: String,
-    commit_url: String,
-}
 
 #[rocket::get("/?<page>&<q>")]
 fn index(
@@ -121,10 +108,11 @@ fn run_db_migrations(rocket: rocket::Rocket) -> Result<Rocket, Rocket> {
 fn main() {
     dotenv::dotenv().ok();
 
+    let current_dir = env::current_dir().unwrap().to_str().unwrap().to_owned();
+
     rocket::ignite()
         .attach(DatabaseConnection::fairing())
         .attach(Template::custom(|engines| {
-            engines.tera.register_function("build_info", build_info());
             engines
                 .tera
                 .register_function("get_thumbnail_url", context::get_thumbnail_url());
@@ -169,31 +157,8 @@ fn main() {
         )
         .mount(
             "/public",
-            StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/build")),
+            StaticFiles::from(format!("{}/{}", current_dir, "build")),
         )
         .register(rocket::catchers![not_found])
         .launch();
-}
-
-/// Template function that returns information about the current release build.
-fn build_info() -> GlobalFn {
-    Box::new(move |_args| -> TeraResult<TeraValue> {
-        let sha = env::var("VERGEN_SHA").unwrap();
-
-        let build_info = BuildInfo {
-            build_timestamp: env::var("VERGEN_BUILD_TIMESTAMP").unwrap(),
-            build_date: env::var("VERGEN_BUILD_DATE").unwrap(),
-            sha_short: env::var("VERGEN_SHA_SHORT").unwrap(),
-            commit_date: env::var("VERGEN_COMMIT_DATE").unwrap(),
-            target_triple: env::var("VERGEN_TARGET_TRIPLE").unwrap(),
-            semver: env::var("VERGEN_SEMVER").unwrap(),
-            commit_url: format!(
-                "https://github.com/andrewvy/spin-archive.org/commit/{}",
-                &sha
-            ),
-            sha,
-        };
-
-        Ok(serde_json::to_value(build_info).unwrap())
-    })
 }
